51e937cc005c8163d22c06a9779cc0bbe26c5dbd
dhantosy <dhantosy@gmail.com>
2019-03-08 10:58:38 +0700

    update kegiatan sosial in form step 2, added tambah and hapus button for form elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,4 +29,31 @@ $(document).ready(function () {
     }
   });
 
+  var current_id = 1;
+  $('#tambah-kegiatan-sosial').on("click", function() {
+    nextElement($('#kegiatanSosialForm_1'));
+    return false;
+  })
+  
+  function nextElement(element){ 
+    if (current_id !== 9) {
+      var newElement = element.clone();
+      var id = current_id + 1;
+      var field = $('input', newElement).attr("id");
+
+      current_id = id;
+      element.data('count', id);     
+      newElement.attr("id",element.attr("id").split("_")[0]+"_"+id);
+      newElement.data('id',id);
+      
+      $('input', newElement).attr("id", field.split("_")[0]+"_"+id );
+      $('select', newElement).attr("id", field.split("_")[0]+"_"+id );
+      newElement.appendTo(element.parent());
+    }
+  }
+
+  $('body').on("click", '.btn__removeClone', function() {
+    $(this).parents('.can-clone').remove();
+  });
+
 });
